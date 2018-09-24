28957b4f5b4202aaf769b4f703207799943f9e56
pillar <pillar@appledeMacBook-Pro.local>
2018-09-24 17:20:38 +0800

    update

--- mailHelp_test.go
+++ mailHelp_test.go
@@ -1,13 +1,15 @@
 package helps
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestMail(t *testing.T) {
-	var d data
-	SendMail()
-	// if d.Name != "dog" && d.Age != 10 {
-	// 	t.Errorf(`TestString`)
-	// }
+	userMail := "ymagain@qq.com"
+	body := fmt.Sprintf(`
+	<p>If your need change WorkOS.top password, please click this button Within 48 hours. </p>
+	<a href="http://workos.top/change-password/%s/%s/" >Change My Password</a>
+`, userMail, "token123")
+	SendMail("xxx@163.com", "passwordxxx", "smtp.163.com:25", userMail, "WorkOS Change Password", body, "html")
 }
